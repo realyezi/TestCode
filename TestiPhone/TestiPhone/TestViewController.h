a1c51b6c948799813cbe2fd3ebf435e17624a9e4
lixia <lixia@videbo.com>
2015-11-15 13:32:36 +0800

    自动化布局中的AutoLayout配置

---
+++ TestiPhone/TestiPhone/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  TestiPhone
+//
+//  Created by realyezi on 15/11/15.
+//  Copyright © 2015年 李 玥瑶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
